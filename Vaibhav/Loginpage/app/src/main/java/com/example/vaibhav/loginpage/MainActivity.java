49d0021e1b20406a138b4f2e356a792e702fe54a
vaibhav <vbisht164@gmail.com>
2017-06-27 19:11:31 +0530

    Added my folder

---
+++ Vaibhav/Loginpage/app/src/main/java/com/example/vaibhav/loginpage/MainActivity.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017. Relsell Global
+ */
+
+package com.example.vaibhav.loginpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.vaibhav.loginpage.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    }
+
