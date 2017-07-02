344d3a0d0771fdd74f114d03d9180d98c8236773
Relsell <relsellglobal@gmail.com>
2017-07-03 03:29:16 +0530

    added admob

--- ColorPickerDemo/app/src/main/java/in/relsellglobal/colorpickerdemo/MainActivityCustomView.java
+++ ColorPickerDemo/app/src/main/java/in/relsellglobal/colorpickerdemo/MainActivityCustomView.java
@@ -8,10 +8,14 @@ import android.Manifest;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -32,6 +36,11 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -56,6 +65,11 @@ public class MainActivityCustomView extends AppCompatActivity {
 
     RelativeLayout root;
 
+    private InterstitialAd mInterstitialAd;
+
+    public static String sharedPrefsFile = "perfs";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,13 +82,56 @@ public class MainActivityCustomView extends AppCompatActivity {
         takePictureButton = (Button) findViewById(R.id.cameraBtn);
         cursorImageView = (ImageView) findViewById(R.id.cursorimage);
         contentLayout = (FrameLayout) findViewById(R.id.content);
-        tosTV = (TextView)findViewById(R.id.tosTV);
+        tosTV = (TextView) findViewById(R.id.tosTV);
+
+
+        if (BuildConfig.DEBUG) {
+            // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+            MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        } else {
+
+            WifiManager wifiManager = (WifiManager) MainActivityCustomView.this.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState()) == NetworkInfo.DetailedState.CONNECTED) {
+                String ssid = wifiInfo.getSSID();
+                if (!ssid.contains("TILLU")) {
+                    MobileAds.initialize(this, "ca-app-pub-3676840300761048~6667031041");
+                } else {
+                    MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+                }
+
+            }
+
+
+        }
+        mInterstitialAd = new InterstitialAd(this);
+
+        if (BuildConfig.DEBUG) {
+            mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        } else {
+
+            WifiManager wifiManager = (WifiManager) MainActivityCustomView.this.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState()) == NetworkInfo.DetailedState.CONNECTED) {
+                String ssid = wifiInfo.getSSID();
+                if (!ssid.contains("TILLU")) {
+                    // production id
+                    mInterstitialAd.setAdUnitId("ca-app-pub-3676840300761048/8143764249");
+                } else {
+                    mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+                }
+
+            }
+
+
+        }
+
 
         tosTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent i = new Intent(MainActivityCustomView.this,TOSActivity.class);
+                Intent i = new Intent(MainActivityCustomView.this, TOSActivity.class);
                 startActivity(i);
 
             }
@@ -125,43 +182,42 @@ public class MainActivityCustomView extends AppCompatActivity {
                     case DragEvent.ACTION_DRAG_ENDED:
                         // Dropped, reassign View to ViewGroup
                         try {
-                        View view = (View) event.getLocalState();
+                            View view = (View) event.getLocalState();
 
-                        view.setVisibility(View.VISIBLE);
+                            view.setVisibility(View.VISIBLE);
 
-                        if (selectedImage != null) {
+                            if (selectedImage != null) {
 
-                            selectedImage.setDrawingCacheEnabled(true);
+                                selectedImage.setDrawingCacheEnabled(true);
 
-                            selectedImage.buildDrawingCache();
+                                selectedImage.buildDrawingCache();
 
-                            selectedImage.setImageBitmap(bitmap);
+                                selectedImage.setImageBitmap(bitmap);
 
-                            bitmap = selectedImage.getDrawingCache();
-                        } else {
-                            return true;
-                        }
-
-
-                        if (evX > 0 && evY > 0 && evX < bitmap.getWidth() && evY < bitmap.getHeight()) {
-                            int pxl = bitmap.getPixel(evX, evY);
-                            int r1 = Color.red(pxl);
-                            int g1 = Color.green(pxl);
-                            int b1 = Color.blue(pxl);
-                            int alpha1 = Color.alpha(pxl);
+                                bitmap = selectedImage.getDrawingCache();
+                            } else {
+                                return true;
+                            }
 
 
-                            Log.v("TAG", "R G B " + r1 + " " + g1 + " " + b1);
+                            if (evX > 0 && evY > 0 && evX < bitmap.getWidth() && evY < bitmap.getHeight()) {
+                                int pxl = bitmap.getPixel(evX, evY);
+                                int r1 = Color.red(pxl);
+                                int g1 = Color.green(pxl);
+                                int b1 = Color.blue(pxl);
+                                int alpha1 = Color.alpha(pxl);
 
-                            final StringBuilder builder1 = new StringBuilder();
-                            builder1.append("#");
-                            builder1.append(r1 > 9 ? Integer.toHexString(r1) : "0" + Integer.toHexString(r1)); // Real computation here
-                            builder1.append(g1 > 9 ? Integer.toHexString(g1) : "0" + Integer.toHexString(g1)); // Real computation here
-                            builder1.append(b1 > 9 ? Integer.toHexString(b1) : "0" + Integer.toHexString(b1)); // Real computation here
 
-                            Log.v("TAG", "Hex Color is " + builder1.toString());
-                            //Toast.makeText(MainActivity.this,"Selected Color is "+builder.toString(),Toast.LENGTH_LONG).show();
+                                Log.v("TAG", "R G B " + r1 + " " + g1 + " " + b1);
 
+                                final StringBuilder builder1 = new StringBuilder();
+                                builder1.append("#");
+                                builder1.append(r1 > 9 ? Integer.toHexString(r1) : "0" + Integer.toHexString(r1)); // Real computation here
+                                builder1.append(g1 > 9 ? Integer.toHexString(g1) : "0" + Integer.toHexString(g1)); // Real computation here
+                                builder1.append(b1 > 9 ? Integer.toHexString(b1) : "0" + Integer.toHexString(b1)); // Real computation here
+
+                                Log.v("TAG", "Hex Color is " + builder1.toString());
+                                //Toast.makeText(MainActivity.this,"Selected Color is "+builder.toString(),Toast.LENGTH_LONG).show();
 
 
                                 dropper.setBackgroundColor(Color.parseColor(builder1.toString()));
@@ -187,13 +243,54 @@ public class MainActivityCustomView extends AppCompatActivity {
 
         //root = (RelativeLayout) findViewById(R.id.activity_main);
 
+        clickHandlerCode();
 
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!mInterstitialAd.isLoaded()) {
+            mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        }
+    }
+
+    public void clickHandlerCode() {
         galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, 1);
+                SharedPreferences sharedpreferences = getSharedPreferences(sharedPrefsFile, Context.MODE_PRIVATE);
+                int value = sharedpreferences.getInt("gallery_click", 0);
+
+                if (value >= 3) {
+                    if (mInterstitialAd.isLoaded()) {
+                        mInterstitialAd.show();
+                        value = 0;
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putInt("gallery_click", value);
+                        editor.commit();
+                    } else {
+                        Log.d("TAG", "The interstitial wasn't loaded yet.");
+                        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                        photoPickerIntent.setType("image/*");
+                        startActivityForResult(photoPickerIntent, 1);
+                        value++;
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putInt("gallery_click", value);
+                        editor.commit();
+                    }
+                } else {
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, 1);
+                    value++;
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putInt("gallery_click", value);
+                    editor.commit();
+                }
+
 
             }
         });
@@ -223,9 +320,42 @@ public class MainActivityCustomView extends AppCompatActivity {
             }
         });
 
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.i("Ads", "onAdLoaded");
 
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Log.i("Ads", "onAdFailedToLoad");
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+                Log.i("Ads", "onAdOpened");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Log.i("Ads", "onAdLeftApplication");
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the interstitial ad is closed.
+                Log.i("Ads", "onAdClosed");
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 1);
+            }
+        });
     }
-
 
     public void callCamera() {
 
