9067a333ecaf572bac936f669fd06de85eb754fb
Ankit <ankitel2929@gmail.com>
2017-07-06 18:56:08 +0530

    added

--- Ankit/JSONParsingDemo/app/src/main/java/in/relsellglobal/jsonparsingdemo/ItemFragment.java
+++ Ankit/JSONParsingDemo/app/src/main/java/in/relsellglobal/jsonparsingdemo/ItemFragment.java
@@ -5,6 +5,7 @@
 package in.relsellglobal.jsonparsingdemo;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -13,10 +14,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import in.relsellglobal.jsonparsingdemo.dummy.DummyContent;
 import in.relsellglobal.jsonparsingdemo.dummy.DummyContent.DummyItem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,6 +44,7 @@ public class ItemFragment extends Fragment {
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
     private int mColumnCount = 1;
+    private ProgressBar pb;
     private OnListFragmentInteractionListener mListener;
 
     List<WebPojo> webPojoList;
@@ -76,7 +90,9 @@ public class ItemFragment extends Fragment {
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
+            webPojoList = new ArrayList<>();
             myItemRecyclerViewAdapter = new MyItemRecyclerViewAdapter(webPojoList, mListener);
+            new JSONParsingTask().execute();
             recyclerView.setAdapter(myItemRecyclerViewAdapter );
         }
         return view;
@@ -113,5 +129,79 @@ public class ItemFragment extends Fragment {
     public interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
         void onListFragmentInteraction(DummyItem item);
+    }
+    public class JSONParsingTask extends AsyncTask<Void,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pb.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String url = "http://relsellglobal.in/DeliveryS/CRKInsightsServer/mobilecheck.php?controlVar=32";
+            StringBuffer stringBuffer = new StringBuffer();
+
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+
+                int code = connection.getResponseCode();
+
+                if(code == HttpURLConnection.HTTP_OK) {
+
+                    InputStream inputStream = connection.getInputStream();
+
+                    String line = "";
+
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    while((line = bufferedReader.readLine()) != null) {
+                        stringBuffer.append(line);
+                    }
+
+                    return stringBuffer.toString();
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if(s!=null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    JSONObject jsonObjectList = jsonObject.getJSONObject("list");
+                    JSONArray jsonArray = jsonObjectList.getJSONArray("data");
+
+                    for(int i=0;i<jsonArray.length();i++) {
+                        JSONObject obj = (JSONObject) jsonArray.get(i);
+                        WebPojo webPojo = new WebPojo();
+                        webPojo.setId(obj.getString("ID"));
+                        webPojo.setUsername(obj.getString("USERNAME"));
+                        webPojo.setFcmId(obj.getString("FCMID"));
+                        webPojoList.add(webPojo);
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(webPojoList);
+                myItemRecyclerViewAdapter.notifyDataSetChanged();
+
+            }
+
+
+        }
     }
 }
--- Ankit/JSONParsingDemo/app/src/main/java/in/relsellglobal/jsonparsingdemo/MainActivity.java
+++ Ankit/JSONParsingDemo/app/src/main/java/in/relsellglobal/jsonparsingdemo/MainActivity.java
@@ -5,6 +5,8 @@
 package in.relsellglobal.jsonparsingdemo;
 
 import android.os.AsyncTask;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -31,9 +33,12 @@ public class MainActivity extends AppCompatActivity implements ItemFragment.OnLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webPojoArrayList = new ArrayList<>();
 
-        new JSONParsingTask().execute();
+
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.activity_main,new ItemFragment());
+        ft.commit();
     }
 
     @Override
@@ -41,75 +46,7 @@ public class MainActivity extends AppCompatActivity implements ItemFragment.OnLi
 
     }
 
-    public class JSONParsingTask extends AsyncTask<Void,Void,String> {
 
-        @Override
-        protected String doInBackground(Void... params) {
-
-            String url = "http://relsellglobal.in/DeliveryS/CRKInsightsServer/mobilecheck.php?controlVar=32";
-            StringBuffer stringBuffer = new StringBuffer();
-
-            try {
-                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-
-                int code = connection.getResponseCode();
-
-                if(code == HttpURLConnection.HTTP_OK) {
-
-                    InputStream inputStream = connection.getInputStream();
-
-                    String line = "";
-
-
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                    while((line = bufferedReader.readLine()) != null) {
-                        stringBuffer.append(line);
-                    }
-
-                    return stringBuffer.toString();
-
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            if(s!=null) {
-                try {
-                    JSONObject jsonObject = new JSONObject(s);
-                    JSONObject jsonObjectList = jsonObject.getJSONObject("list");
-                    JSONArray jsonArray = jsonObjectList.getJSONArray("data");
-
-                    for(int i=0;i<jsonArray.length();i++) {
-                        JSONObject obj = (JSONObject) jsonArray.get(i);
-                        WebPojo webPojo = new WebPojo();
-                        webPojo.setId(obj.getString("ID"));
-                        webPojo.setUsername(obj.getString("USERNAME"));
-                        webPojo.setFcmId(obj.getString("FCMID"));
-                        webPojoArrayList.add(webPojo);
-                    }
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println(webPojoArrayList);
-
-
-            }
-
-
-        }
-    }
 
 
 }
